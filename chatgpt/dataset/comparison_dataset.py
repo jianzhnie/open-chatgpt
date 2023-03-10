70f2943cc7fec1def8656a041fec10798f7a110e
jianzhnie <jianzhnie@126.com>
2023-03-10 20:25:41 +0800

    update datasets class

--- chatgpt/dataset/comparison_dataset.py
+++ chatgpt/dataset/comparison_dataset.py
@@ -1,52 +1,91 @@
+from typing import Dict, List, Tuple
+
 import torch
 from torch.utils.data import Dataset
-from tqdm import tqdm
+from transformers import PreTrainedTokenizer
 
 
 class PairwiseDataset(Dataset):
-    def __init__(self, pairs, tokenizer, max_length):
-        self.chosen_input_ids = []
-        self.chosen_attn_masks = []
-        self.rejected_input_ids = []
-        self.rejected_attn_masks = []
-        for pair in tqdm(pairs):
-            chosen, rejected = pair['chosen'], pair['rejected']
-            chosen_encodings_dict = tokenizer(
-                '<|startoftext|>' + chosen + '<|endoftext|>',
+    """Dataset class for pairwise ranking tasks.
+
+    Args:
+        pairs: List of dictionaries containing 'positive' and 'negative' keys.
+        tokenizer: The tokenizer used to encode the input text.
+        max_length: Maximum sequence length for the encoded inputs.
+    """
+    def __init__(self, pairs: List[Dict[str, str]],
+                 tokenizer: PreTrainedTokenizer, max_length: int):
+        self.positive_input_ids = []
+        self.positive_attn_masks = []
+        self.negative_input_ids = []
+        self.negative_attn_masks = []
+
+        for pair in pairs:
+            positive_example, negative_example = pair['positive'], pair[
+                'negative']
+            positive_encodings_dict = tokenizer(
+                positive_example,
                 truncation=True,
                 max_length=max_length,
                 padding='max_length',
                 return_tensors='pt',
             )
-            rejected_encodings_dict = tokenizer(
-                '<|startoftext|>' + rejected + '<|endoftext|>',
+            negative_encodings_dict = tokenizer(
+                negative_example,
                 truncation=True,
                 max_length=max_length,
                 padding='max_length',
                 return_tensors='pt',
             )
-            self.chosen_input_ids.append(chosen_encodings_dict['input_ids'])
-            self.chosen_attn_masks.append(
-                chosen_encodings_dict['attention_mask'])
-            self.rejected_input_ids.append(
-                rejected_encodings_dict['input_ids'])
-            self.rejected_attn_masks.append(
-                rejected_encodings_dict['attention_mask'])
+            if not positive_encodings_dict or not negative_encodings_dict:
+                raise ValueError('Empty encoding dictionary.')
+            self.positive_input_ids.append(
+                positive_encodings_dict['input_ids'])
+            self.positive_attn_masks.append(
+                positive_encodings_dict['attention_mask'])
+            self.negative_input_ids.append(
+                negative_encodings_dict['input_ids'])
+            self.negative_attn_masks.append(
+                negative_encodings_dict['attention_mask'])
 
-    def __len__(self):
-        return len(self.chosen_input_ids)
+    def __len__(self) -> int:
+        return len(self.positive_input_ids)
 
-    def __getitem__(self, idx):
-        return (self.chosen_input_ids[idx], self.chosen_attn_masks[idx],
-                self.rejected_input_ids[idx], self.rejected_attn_masks[idx])
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+        return {
+            'positive_input_ids': torch.tensor(self.positive_input_ids[idx]),
+            'positive_attention_mask':
+            torch.tensor(self.positive_attn_masks[idx]),
+            'negative_input_ids': torch.tensor(self.negative_input_ids[idx]),
+            'negative_attention_mask':
+            torch.tensor(self.negative_attn_masks[idx])
+        }
 
 
-class DataCollatorReward:
-    def __call__(self, data):
-        batch = {}
-        batch['input_ids'] = torch.cat([f[0]
-                                        for f in data] + [f[2] for f in data])
-        batch['attention_mask'] = torch.cat([f[1] for f in data] +
-                                            [f[3] for f in data])
-        batch['labels'] = torch.tensor([0] * len(data) + [1] * len(data))
+class RewardDataCollator:
+    """A data collator for binary classification tasks.
+
+    Args:
+        data (list): A list of tuples containing tensors of the same size.
+    Returns:
+        A dictionary containing concatenated input tensors and labels.
+    """
+    def __call__(self,
+                 data: List[Tuple[torch.Tensor]]) -> Dict[str, torch.Tensor]:
+        # Check that the input data is a list of tuples containing tensors of the same size
+        if not isinstance(data, list) or len(data) == 0:
+            raise ValueError('Input data must be a non-empty list.')
+
+        # Concatenate the input tensors using default_collate
+        input_ids = torch.cat([x[0] for x in data] + [x[2] for x in data])
+        attention_masks = torch.cat([x[1]
+                                     for x in data] + [x[3] for x in data])
+        labels = torch.cat([torch.zeros(len(data)), torch.ones(len(data))])
+
+        batch = {
+            'input_ids': input_ids,
+            'attention_mask': attention_masks,
+            'labels': labels
+        }
+
         return batch
--- chatgpt/dataset/summarize_dataset.py
+++ chatgpt/dataset/summarize_dataset.py
@@ -1,9 +1,11 @@
 import json
+from typing import Any, Dict, Union
 
 import pandas as pd
 import torch
 from datasets import load_dataset
 from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer
 
 
 def get_dataset_from_jsonl(jsonl_file, return_summary=True):
@@ -26,42 +28,81 @@ def get_dataset_from_jsonl(jsonl_file, return_summary=True):
 
 
 class TLDRDataset(Dataset):
-    def __init__(self, train_path, tokenizer, split, max_length=550):
-        self.post_list = []
+    """A PyTorch Dataset for TLDR training data.
+
+    Args:
+        train_path (str): Path to the training data.
+        tokenizer (PreTrainedTokenizer): The tokenizer to use.
+        split (str): The split to use from the training data.
+        max_length (int): The maximum length of the input sequences (default: 550).
+    """
+    def __init__(self,
+                 train_path: str,
+                 tokenizer: PreTrainedTokenizer,
+                 split: str,
+                 max_length: int = 550) -> None:
+
         dataset = load_dataset(train_path, split=split)
-        for sample in dataset:
-            self.post_list.append(sample['prompt'] + sample['label'])
+        self.post_list = [(sample['prompt'], sample['label'])
+                          for sample in dataset]
         if 'valid' in train_path:
             self.post_list = self.post_list[0:2000]
         self.tokenizer = tokenizer
         self.max_length = max_length
-        self.input_ids = []
-        self.attn_masks = []
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.post_list)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> Dict[str, Any]:
+        """Returns a dictionary containing the input_ids, attention_mask, and
+        labels for the given index.
+
+        Args:
+            idx (int): The index of the data sample to retrieve.
+
+        Returns:
+            A dictionary containing the input_ids, attention_mask, and labels.
+        """
+        if idx < 0 or idx >= len(self.post_list):
+            raise IndexError(
+                f'Index {idx} out of range for TLDRDataset with length {len(self)}'
+            )
+
         txt = self.post_list[idx]
         encodings_dict = self.tokenizer(txt,
                                         truncation=True,
                                         max_length=self.max_length,
                                         padding='max_length')
         input_ids = torch.tensor(encodings_dict['input_ids'])
-        attn_masks = torch.tensor(encodings_dict['attention_mask'])
+        attn_mask = torch.tensor(encodings_dict['attention_mask'])
+        labels = input_ids.clone()
 
         return {
             'input_ids': input_ids,
-            'attention_mask': attn_masks,
-            'labels': input_ids
+            'attention_mask': attn_mask,
+            'labels': labels
         }
 
 
 class ComparisonDataset(Dataset):
-    def __init__(self, comparison_path, tokenizer, max_length=550):
+    """A PyTorch Dataset class for generating input sequences for training a
+    text comparison model.
+
+    Args:
+        comparison_path (str): Path to the input JSON file.
+        tokenizer (tokenizer): A Hugging Face tokenizer object.
+        max_length (int, optional): Maximum sequence length for the model.
+            Defaults to 550.
+    """
+    def __init__(self,
+                 comparison_path: str,
+                 tokenizer: PreTrainedTokenizer,
+                 max_length: int = 550):
+        # Load the input JSON file into memory
         with open(comparison_path, 'r') as f:
             dataset = [json.loads(line) for line in f]
 
+        # Initialize the class variables
         self.tokenizer = tokenizer
         self.post_list = []
         self.summaries_0 = []
@@ -69,28 +110,38 @@ class ComparisonDataset(Dataset):
         self.labels = []
         self.max_length = max_length
 
-        def make_text(post, summarize):
+        # Define a helper function to create the input sequence text
+        def make_text(post: Dict[str, Union[str, Dict[str, str]]],
+                      summarize: str) -> str:
             return f"SUBREDDIT: r/{post['subreddit']}\nTITLE: {post['title']}\nPOST: {post['post']}\nTL;DR: {summarize}"
 
-        for sample in dataset:  # chosen summary is always the first one
+        # Parse the JSON input data and generate the input sequences
+        for sample in dataset:
+            # Add the post to the post list
             self.post_list.append(sample['info']['post'])
+
             # NOTE: The chosen summary is always the first one, i.e. `sample["summaries"][0]`
             if sample['choice'] == 0:
+                # Add the first summary to summaries_0 and the second summary to summaries_1
                 self.summaries_0.append(
                     make_text(sample['info'], sample['summaries'][0]['text']))
                 self.summaries_1.append(
                     make_text(sample['info'], sample['summaries'][1]['text']))
             else:
+                # Add the second summary to summaries_0 and the first summary to summaries_1
                 self.summaries_0.append(
                     make_text(sample['info'], sample['summaries'][1]['text']))
                 self.summaries_1.append(
                     make_text(sample['info'], sample['summaries'][0]['text']))
+
+            # Add the label (always 0) to the labels list
             self.labels.append(0)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.post_list)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+        # Get the two summaries to compare at the current index
         summ0 = self.summaries_0[idx]
         summ1 = self.summaries_1[idx]
         encodings_dict = self.tokenizer(
@@ -105,33 +156,45 @@ class ComparisonDataset(Dataset):
 
 
 class AllSummDataset(Dataset):
-    def __init__(self, train_path, tokenizer, split, max_length=1024):
-        df = pd.read_parquet(train_path)
+    def __init__(self,
+                 data_path: str,
+                 tokenizer: PreTrainedTokenizer,
+                 split: str = 'train',
+                 max_length: int = 1024):
+        """
+        Args:
+            data_path (str): Path to the data file.
+            tokenizer (Tokenizer): Tokenizer object.
+            split (str): Either "train" or "valid".
+            max_length (int): Maximum sequence length.
+        """
+        if split not in ['train', 'valid']:
+            raise ValueError(
+                "Invalid split. Must be either 'train' or 'valid'.")
+
+        df = pd.read_parquet(data_path)
         if split == 'valid':
             df = df.sample(n=5000)
-        self.summarizes = []
-        for i, row in df.iterrows():
-            self.summarizes.append(
-                f"Summarize: {row['text']}. TL;DR: {row['summary']}")
+
+        self.summaries = [
+            f"Summarize: {row['text']}. TL;DR: {row['summary']}"
+            for i, row in df.iterrows()
+        ]
+
         self.tokenizer = tokenizer
         self.max_length = max_length
-        self.input_ids = []
-        self.attn_masks = []
 
-    def __len__(self):
-        return len(self.summarizes)
+    def __len__(self) -> int:
+        return len(self.summaries)
 
-    def __getitem__(self, idx):
-        txt = self.summarizes[idx]
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+        txt = self.summaries[idx]
         encodings_dict = self.tokenizer(txt,
                                         truncation=True,
                                         max_length=self.max_length,
                                         padding='max_length')
-        input_ids = torch.tensor(encodings_dict['input_ids'])
-        attn_masks = torch.tensor(encodings_dict['attention_mask'])
 
-        return {
-            'input_ids': input_ids,
-            'attention_mask': attn_masks,
-            'labels': input_ids
-        }
+        input_ids = torch.tensor(encodings_dict['input_ids'])
+        attn_mask = torch.tensor(encodings_dict['attention_mask'])
+
+        return {'input_ids': input_ids, 'attention_mask': attn_mask}
