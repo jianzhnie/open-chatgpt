4c7ae46d66d0ec9e574005b1dd645876ff5b1d7d
jianzhnie <jianzhnie@126.com>
2023-03-13 16:39:57 +0800

    add reward model

--- chatgpt/rlhf/pairwise_loss.py
+++ chatgpt/rlhf/pairwise_loss.py
@@ -3,30 +3,26 @@ import torch.nn as nn
 
 
 class PairWiseLoss(nn.Module):
-    """
-    Pairwise Loss for Ranking Tasks
-    
-    This PyTorch module computes a pairwise loss for ranking tasks where the goal is to compare 
-    two inputs and determine which one is "better" than the other. Given two input tensors: 
-    `chosen_reward` and `reject_reward`, which should contain reward values for the "chosen" 
-    and "rejected" options, respectively, this module computes the probability of the chosen 
-    option being "better" than the rejected option using a sigmoid function, and then takes 
-    the negative logarithm of that probability to get the loss. The loss is then averaged over 
-    the batch dimension and returned as a scalar tensor. Note that this module assumes that 
-    higher reward values indicate better options.
+    """Pairwise Loss for Ranking Tasks.
+
+    This PyTorch module computes a pairwise loss for ranking tasks where the goal is to compare two inputs and determine
+    which one is "better" than the other. Given two input tensors: `chosen_reward` and `reject_reward`, which should
+    contain reward values for the "chosen" and "rejected" options, respectively, this module computes the probability of
+    the chosen option being "better" than the rejected option using a sigmoid function, and then takes the negative
+    logarithm of that probability to get the loss. The loss is then averaged over the batch dimension and returned as a
+    scalar tensor. Note that this module assumes that higher reward values indicate better options.
     """
     def __init__(self):
         super(PairWiseLoss, self).__init__()
 
     def forward(self, chosen_reward: torch.Tensor,
                 reject_reward: torch.Tensor) -> torch.Tensor:
-        """
-        Compute pairwise loss
-        
+        """Compute pairwise loss.
+
         Args:
         - chosen_reward: A tensor of shape (batch_size,) containing reward values for the chosen option
         - reject_reward: A tensor of shape (batch_size,) containing reward values for the rejected option
-        
+
         Returns:
         - loss: A scalar tensor containing the computed pairwise loss
         """
--- chatgpt/rlhf/reward_model.py
+++ chatgpt/rlhf/reward_model.py
@@ -1,20 +1,35 @@
+from dataclasses import dataclass
+from typing import Optional
+
 import torch
 from torch import nn
-from transformers import OPTModel
-from transformers import GPT2Model
-from transformers import BloomModel
-from typing import Optional
+from transformers import BloomModel, GPT2Model, OPTModel
+from transformers.modeling_outputs import ModelOutput
+
+from .pairwise_loss import PairWiseLoss
+
+
+@dataclass
+class RewardModelOutput(ModelOutput):
+    """Base class for outputs of sentence classification models.
+
+    Args:
+        loss (`torch.FloatTensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+            Classification (or regression if config.num_labels==1) loss.
+    """
+
+    loss: Optional[torch.FloatTensor] = None
+    rewards_chosen: torch.FloatTensor = None
+    rewards_rejected: torch.FloatTensor = None
 
 
 class RewardModel(nn.Module):
-    """
-    GPT Reward model.
+    """GPT Reward model.
 
     Args:
         model (str): Model name: 'opt', 'gpt2' or 'bloom'
         pretrained (str): Pretrained model name or path.
     """
-
     def __init__(self, model: str = '', pretrained: str = 'openai-gpt'):
         super().__init__()
         # Instantiate model based on input string
@@ -32,12 +47,17 @@ class RewardModel(nn.Module):
         # Get the model's config and create a value head
         self.config = self.model.config
         self.value_head = nn.Linear(self.config.word_embed_proj_dim, 1)
+        self.loss_fn = PairWiseLoss()
 
-    def forward(self,
-                input_ids: torch.LongTensor,
-                attention_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
-        """
-        Forward pass of the model.
+    def forward(
+        self,
+        chosen_input_ids: torch.LongTensor,
+        rejected_input_ids: torch.LongTensor,
+        chosen_attention_mask: Optional[torch.Tensor] = None,
+        rejected_attention_mask: Optional[torch.Tensor] = None,
+        return_dict=None,
+    ) -> torch.Tensor:
+        """Forward pass of the model.
 
         Args:
             input_ids (torch.LongTensor): Input tensor of token ids.
@@ -46,12 +66,34 @@ class RewardModel(nn.Module):
         Returns:
             torch.Tensor: Output tensor of value estimates.
         """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
         # Get the model's outputs and extract the last hidden state
-        outputs = self.model(input_ids, attention_mask=attention_mask)
-        last_hidden_states = outputs['last_hidden_state']
+        outputs_chosen = self.model(input_ids=chosen_input_ids,
+                                    attention_mask=chosen_attention_mask,
+                                    return_dict=return_dict)
+        last_hidden_states_chosen = outputs_chosen['last_hidden_state']
 
         # Calculate the values and return the mean value for each sequence
-        values = self.value_head(last_hidden_states)[:, :-1]
-        value = values.mean(dim=1).squeeze(1)
+        values_chosen = self.value_head(last_hidden_states_chosen)[:, :-1]
+        rewards_chosen = values_chosen.mean(dim=1).squeeze(1)
 
-        return value
+        # Get the model's outputs and extract the last hidden state
+
+        outputs_rejected = self.model(input_ids=rejected_input_ids,
+                                      attention_mask=rejected_attention_mask,
+                                      return_dict=return_dict)
+        last_hidden_states_rejected = outputs_rejected['last_hidden_state']
+
+        # Calculate the values and return the mean value for each sequence
+        values_rejected = self.value_head(last_hidden_states_rejected)[:, :-1]
+        rewards_rejected = values_rejected.mean(dim=1).squeeze(1)
+
+        loss = self.loss_fn(rewards_chosen, rewards_rejected)
+
+        if not return_dict:
+            output = (loss, ) + rewards_chosen + rewards_rejected
+            return output
+
+        return RewardModelOutput(loss=loss,
+                                 rewards_chosen=rewards_chosen,
+                                 rewards_rejected=rewards_rejected)
