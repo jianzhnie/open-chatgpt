99da80bf6be1e90c60f91cf1de0b4dd2688b65c1
jianzhnie <jianzhnie@126.com>
2023-03-14 10:42:32 +0800

    update reward model

--- scripts/train_reward_model.py
+++ scripts/train_reward_model.py
@@ -1,6 +1,7 @@
 import os
 import sys
-
+import evaluate
+import numpy as np
 from transformers import (AutoTokenizer, Trainer, TrainingArguments,
                           default_data_collator)
 
@@ -18,20 +19,20 @@ if __name__ == '__main__':
     training_args = TrainingArguments(
         output_dir='rm_checkpoint/',
         num_train_epochs=5,
-        logging_steps=10,
+        logging_steps=1,
         gradient_accumulation_steps=2,
         save_strategy='steps',
         evaluation_strategy='steps',
         per_device_train_batch_size=8,
         per_device_eval_batch_size=8,
         eval_accumulation_steps=1,
-        eval_steps=500,
+        eval_steps=1,
         save_steps=500,
         warmup_steps=100,
         fp16=True,
         logging_dir='./logs',
         learning_rate=1e-5,
-        save_total_limit=1,
+        save_total_limit=5,
     )
 
     # Initialize the reward model from the (supervised) fine-tuned GPT-J
@@ -42,7 +43,7 @@ if __name__ == '__main__':
     max_length = 550
     train_dataset = PairwiseDataset(data_path,
                                     tokenizer,
-                                    split='train',
+                                    split='valid1',
                                     max_length=max_length)
     val_dataset = PairwiseDataset(data_path,
                                   tokenizer,
