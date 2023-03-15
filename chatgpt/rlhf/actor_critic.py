bf3f84d2ea0565b5a98cca731e203ea27938e41f
jianzhnie <jianzhnie@126.com>
2023-03-15 14:53:06 +0800

    Update actor_critic.py

--- chatgpt/rlhf/actor_critic.py
+++ chatgpt/rlhf/actor_critic.py
@@ -1,97 +1,98 @@
 from typing import Optional, Tuple
-
+from collections import namedtuple
 import torch
 import torch.nn as nn
 from transformers import AutoModel, AutoModelForCausalLM, AutoTokenizer
+from einops.layers.torch import Rearrange
+
+ActionCriticReturn = namedtuple(
+    'ActionCriticReturn',
+    ['actions', 'action_logits', 'values', 'sequence', 'sequences_mask'])
 
 
 class ActorModel(nn.Module):
-    """Actor model.
+    """
+    Actor model that generates logits representing the probability distribution over the vocabulary of actions.
 
     Args:
-        pretrained (str): Pretrained model name or path.
-        config (GPT2Config): Model config.
-        checkpoint (bool): Enable gradient checkpointing.
+        pretrained (str, optional): Pretrained model name or path.
+        debug (bool, optional): Whether to print debug information. Defaults to False.
     """
-    def __init__(self,
-                 pretrained: Optional[str] = None,
-                 debug: bool = False) -> None:
+    def __init__(self, pretrained: Optional[str] = None, debug: bool = False):
         super().__init__()
 
+        # Load tokenizer and set special tokens
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained,
                                                        padding_side='left')
-        # galactica tokenizer eos_token is None
         if self.tokenizer.eos_token is None:
             self.tokenizer.eos_token = '</s>'
             self.tokenizer.eos_token_id = 0
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+
+        # Load pre-trained language model
         self.model = AutoModelForCausalLM.from_pretrained(pretrained)
+
         self.debug = debug
 
-    def forward(self, inputs_ids: torch.Tensor,
+    def forward(self, input_ids: torch.Tensor,
                 attention_mask: torch.Tensor) -> torch.Tensor:
-        """Generate logits to have probability distribution over the vocabulary
-        of the actions.
+        """
+        Generate logits to have probability distribution over the vocabulary of the actions.
 
         Args:
-            sequences (torch.Tensor): Sequences of states and actions used to
-                    compute token logits for the whole list of sequences
-            attention_mask (torch.Tensor): Mask for the sequences attention
+            input_ids (torch.Tensor): Sequences of states and actions used to compute token logits for the whole list of sequences.
+            attention_mask (torch.Tensor): Mask for the sequences attention.
 
         Returns:
-            logits (torch.Tensor): Logits for the actions taken
+            logits (torch.Tensor): Logits for the actions taken.
         """
-        model_output = self.model(inputs_ids, attention_mask=attention_mask)
-        # need to return logits for the actions
-        model_output = model_output.logits
+        model_output = self.model(input_ids, attention_mask=attention_mask)
+        # Extract logits from the model output
+        logits = model_output.logits
+
         if self.debug:
             print('ActorModel.forward')
-            print('model_output_logits shape', model_output.shape)
-            print('model_output logits', model_output)
-        return model_output
+            print('logits shape:', logits.shape)
+            print('logits:', logits)
+
+        return logits
 
     @torch.no_grad()
-    def generate(self, states: torch.Tensor,
-                 state_mask: torch.Tensor) -> Tuple:
+    def generate(self, states: torch.Tensor, state_mask: torch.Tensor,
+                 temperature, max_sequence_length,
+                 max_tokens) -> Tuple[torch.Tensor, torch.Tensor]:
         """Generate actions and sequences=[states, actions] from state (i.e.
         input of the prompt generator model)
 
         Args:
-            state (torch.Tensor): the input of the user
-            state_mask (torch.Tensor): Mask for the state input (for padding)
+            states (torch.Tensor): Input sequence tensor with only state IDs.
+            state_mask (torch.Tensor): Attention mask for input state tensor.
+            temperature (float): Softmax temperature to apply during generation.
+            max_sequence_length (int): Maximum allowed length of generated sequence.
+            max_tokens (int): Maximum number of tokens to generate after input sequence.
 
         Returns:
-            actions (torch.Tensor): Actions generated from the state
-            sequences (torch.Tensor): Sequences generated from the
-                state as [states, actions]
+            Tuple[torch.Tensor, torch.Tensor]: Tuple of generated actions and full generated sequences.
         """
-        temperature = self.config.temperature
-        # max sequence length for the actor (i.e. prompt + completion)
-        # from config file - it depends by the model used
-        max_sequence_length = self.config.max_sequence_length
-        # max tokens generated by the actor (completion only) from config file
-        max_tokens = self.config.max_tokens
-        # temperature for the actor
+        # Set maximum length of generation
         max_generation_possible = max_sequence_length - states.shape[1]
-        # take the minimum between the maximum token that you want to generate
-        # and the token that is possible to generate given the maximum sequence
-        # supported
         max_completion = min(max_tokens, max_generation_possible)
         if max_completion <= 0:
             raise ValueError(
-                'The maximum completion available is <= 0 the prompt is too ' +
-                'long w.r.t the model sequence length')
-        # the max_length is then the input length + the completion length
+                'The maximum completion available is <= 0 the prompt is too long w.r.t the model sequence length'
+            )
         max_length = states.shape[1] + max_completion
-        # generate
+
+        # Generate actions and sequences
         sequences = self.model.generate(
             input_ids=states,
             attention_mask=state_mask,
             temperature=temperature,
             max_length=max_length,
         )
-        actions = sequences[:, states.shape[1]:]  # noqa E203
+        actions = sequences[:, states.shape[
+            1]:]  # Extract generated actions from full sequence
         if self.debug:
             print('ActorModel.generate')
             print('state', states)
@@ -104,78 +105,76 @@ class ActorModel(nn.Module):
 
 
 class CriticModel(nn.Module):
-    """GPT Critic model.
+    """Critic model that evaluates the quality of a given sequence of tokens.
 
     Args:
         pretrained (str): Pretrained model name or path.
-        config (GPT2Config): Model config.
-        checkpoint (bool): Enable gradient checkpointing.
+        debug (bool): Whether to print debugging information or not.
     """
-    def __init__(self,
-                 pretrained: Optional[str] = None,
-                 debug: bool = True) -> None:
+    def __init__(self, pretrained: Optional[str] = None, debug: bool = True):
+        super().__init__()
 
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            pretrained,
-            padding_side='left',
-            truncation_side='left',
-        )
+        # Instantiate tokenizer and model from pretrained checkpoint
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained,
+                                                       padding_side="left",
+                                                       truncation_side="left")
         self.model = AutoModel.from_pretrained(pretrained)
-        # galactica tokenizer eos_token is None
+
+        # Set EOS token and padding token
         if self.tokenizer.eos_token is None:
-            self.tokenizer.eos_token = '</s>'
+            self.tokenizer.eos_token = "</s>"
             self.tokenizer.eos_token_id = 0
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
 
         self.debug = debug
-        # store config
         self.config = self.model.config
-        # initialize the self.model
-        head_hidden_size = self.config.model_head_hidden_size
 
+        # Define value head layers to output a scalar value
+        head_hidden_size = self.config.model_head_hidden_size
         self.value_head = nn.Sequential(
-            torch.nn.Linear(head_hidden_size, head_hidden_size),
-            torch.nn.ReLU(),
-            torch.nn.Linear(head_hidden_size, 1),
-            Rearrange('... 1 -> ...'),
+            nn.Linear(head_hidden_size, head_hidden_size),
+            nn.ReLU(),
+            nn.Linear(head_hidden_size, 1),
+            Rearrange("... 1 -> ..."),
         )
 
     def forward(self, input_ids: torch.Tensor,
                 attention_mask: torch.Tensor) -> torch.Tensor:
-        """Generate the sequence of rewards for the given output sequence what
-        is the quality of the output sequence tokens?
+        """Evaluate the quality of a sequence of tokens.
 
         Args:
-            output_sequence (torch.Tensor): The sequence of tokens to be
-                evaluated
-            output_sequence_mask (torch.Tensor): Mask for the attention
+            input_ids (torch.Tensor): Tensor of token ids of shape (batch_size, seq_length)
+            attention_mask (torch.Tensor): Mask tensor of shape (batch_size, seq_length)
 
         Returns:
-            torch.Tensor: Rewards for the given output sequence
+            torch.Tensor: Tensor of rewards of shape (batch_size, 1)
         """
         output = self.model(input_ids,
                             attention_mask=attention_mask,
                             return_dict=True)
-        # What if the output_sequence is longer than the max context of
-        # the model?
         rewards = self.value_head(output.last_hidden_state)
+
+        # Print debugging information
         if self.debug:
-            print('RewardModel.forward')
-            print('output_sequence.shape', input_ids.shape)
-            print('output_sequence', input_ids)
-            print('reward.shape', rewards.shape)
-            print('reward', rewards)
+            print("CriticModel.forward")
+            print("input_ids.shape", input_ids.shape)
+            print("input_ids", input_ids)
+            print("rewards.shape", rewards.shape)
+            print("rewards", rewards)
+
         return rewards
 
     def get_reward(self, input_ids: torch.Tensor,
                    attention_mask: torch.Tensor) -> torch.Tensor:
-        """Get the reward for the given output sequence.
+        """Get the reward for a sequence of tokens.
 
         Args:
-            output_sequence (torch.Tensor): The concatenation of initial input
-                and actor output as tokens
-            output_sequence_mask (torch.Tensor): Mask for the attention
+            input_ids (torch.Tensor): Tensor of token ids of shape (batch_size, seq_length)
+            attention_mask (torch.Tensor): Mask tensor of shape (batch_size, seq_length)
+
+        Returns:
+            torch.Tensor: Tensor of rewards of shape (batch_size,)
         """
         rewards = self.forward(input_ids, attention_mask)
         return rewards[:, -1]
@@ -186,9 +185,9 @@ class ActorCritic(nn.Module):
     generates values and action for given sequences during the training of the
     actor.
 
-    Attributes:
-        actor (ActorModel): Actor model
-        critic (CriticModel): Critic model
+    Args:
+        actor (nn.Module): Actor model
+        critic (nn.Module): Critic model
         debug (bool): enable prints for Debugging
 
     Methods:
@@ -200,12 +199,12 @@ class ActorCritic(nn.Module):
     """
     def __init__(
         self,
-        actor: nn.Module = None,
-        critic: nn.Module = None,
+        actor: nn.Module,
+        critic: nn.Module,
         debug: bool = False,
-    ) -> None:
+    ):
         super().__init__()
-        self.actor = actor,
+        self.actor = actor
         self.critic = critic
         self.debug = debug
 
@@ -214,15 +213,15 @@ class ActorCritic(nn.Module):
         sequences: torch.Tensor,
         sequences_mask: torch.Tensor,
         action_len: int,
-    ) -> Tuple:
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
         """Given the whole sequences, use the actor forward to get the logits
         for each token in the sequence and the critic forward to get the values
         for each generation step.
 
         Args:
             sequences (torch.Tensor): Sequences composed of [states, actions]
-            sequence_mask (torch.Tensor): Mask for the sequences
-            action_length (int): Length of the actions in the sequences
+            sequences_mask (torch.Tensor): Mask for the sequences
+            action_len (int): Length of the actions in the sequences
 
         Returns:
             action_logits (torch.Tensor): Logits for the actions in the
@@ -232,7 +231,7 @@ class ActorCritic(nn.Module):
         # use a single forward on the whole sequence
         # to get pi(y | x) and ignore predicted output
         actions_logits = self.actor(sequences, sequences_mask)
-        values = self.critic.forward(sequences, sequences_mask)
+        values = self.critic(sequences, sequences_mask)
 
         # return only logits and values for the actions taken
         real_actions_logits = actions_logits[:, -action_len:, :]
@@ -243,43 +242,47 @@ class ActorCritic(nn.Module):
             print('action_len', action_len)
             print('sequences.shape', sequences.shape)
             print('sequences', sequences)
-            print('real_action_logits.shape', actions_logits.shape)
-            print('real_action_logits', actions_logits)
-            print('real_values.shape', values.shape)
-            print('real_values', values)
+            print('real_action_logits.shape', real_actions_logits.shape)
+            print('real_action_logits', real_actions_logits)
+            print('real_values.shape', real_values.shape)
+            print('real_values', real_values)
 
-        return (
-            real_actions_logits,
-            real_values,
-        )
+        return real_actions_logits, real_values
 
     @torch.no_grad()
     def generate(self, states: torch.Tensor,
-                 state_mask: torch.Tensor) -> Tuple:
-        """Generate actions, actions_logits, values and sequences from states.
+                 state_mask: torch.Tensor) -> ActionCriticReturn:
+        """Generate actions, action_logits, values, and sequences from states.
 
         Args:
-            states (torch.Tensor): user inputs
-            state_mask (torch.Tensor): Mask for the states of the environment
+            states (torch.Tensor): The states of the environment.
+            state_mask (torch.Tensor): The mask for the states of the environment.
 
         Returns:
-            actions (torch.Tensor): Actions generated from the states
-            actions_logits (torch.Tensor): Logits for the actions generated
-                from the states (i.e. pi(y | x))
-            values (torch.Tensor): Values generated by the critic model
-                for the actions generated by the actor (i.e. V(x))
-            sequences (torch.Tensor): Sequences generated from the states
-                as [states, actions]
+            ActionCriticReturn: A namedtuple containing the following fields:
+                - actions (torch.Tensor): The generated actions.
+                - actions_logits (torch.Tensor): The logits for the generated actions.
+                - values (torch.Tensor): The values generated by the critic model
+                for the actions generated by the actor.
+                - sequences (torch.Tensor): The sequences generated from the states as
+                [states, actions].
+                - sequences_mask (torch.Tensor): The mask for the generated sequences.
+
         """
-        # generate action sequence
+        # Generate action sequence.
         actions, sequence = self.actor.generate(states, state_mask)
+
+        # Get the mask for the generated sequence.
         sequences_mask = sequence != self.actor.tokenizer.pad_token_id
         sequences_mask = sequences_mask.to(sequence.device).long().detach()
+
+        # Get the length of the generated actions.
         action_len = actions.shape[1]
 
-        # generate actions_logits and values
+        # Generate action logits and values.
         actions_logits, values = self.forward(sequence, sequences_mask,
                                               action_len)
+
         if self.debug:
             print('ActorCritic.generate')
             print('actions shape', actions.shape)
@@ -291,4 +294,5 @@ class ActorCritic(nn.Module):
             print('values shape', values.shape)
             print('values', values)
 
-        return actions, actions_logits, values, sequence, sequences_mask
+        return ActionCriticReturn(actions, actions_logits, values, sequence,
+                                  sequences_mask)
