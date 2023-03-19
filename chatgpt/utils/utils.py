a3ec0f9258d78b34319a78cc7fe2de4119e32b3a
jianzhnie <jianzhnie@126.com>
2023-03-19 22:06:45 +0800

    Update utils.py

--- chatgpt/utils/utils.py
+++ chatgpt/utils/utils.py
@@ -1,14 +1,8 @@
-import random
-
 import numpy as np
-import torch
-import torch.nn.functional as F
 
 
 class LengthSampler:
-    """
-    Samples a length
-    """
+    """Samples a length."""
     def __init__(self, min_value, max_value):
         self.values = list(range(min_value, max_value))
 
