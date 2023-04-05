539fdb76acc447d7778b8285750d2ddc24513163
jianzhnie <jianzhnie@126.com>
2023-04-05 12:01:35 +0800

    Update test_ppo_trainer.py

--- examples/sentiment/test_ppo_trainer.py
+++ examples/sentiment/test_ppo_trainer.py
@@ -1,22 +1,22 @@
+import sys
+
 import torch
 from datasets import load_dataset
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import AutoTokenizer, pipeline
-import sys
-from torch.utils.data import DataLoader
 
-sys.path.append("../../")
-from chatgpt.utils.utils import LengthSampler
+sys.path.append('../../')
 from chatgpt.rlhf.actor_critic import ActorCritic, ActorModel, CriticModel
+from chatgpt.utils.utils import LengthSampler
 
 
 # Below is an example function to build the dataset. In our case, we use the IMDB dataset
 # from the `datasets` library. One should customize this function to train the model on
 # its own dataset.
-def build_dataset(dataset_name="imdb"):
-    """
-    Build dataset for training. This builds the dataset from `load_dataset`, one should
-    customize this function to train the model on its own dataset.
+def build_dataset(dataset_name='imdb'):
+    """Build dataset for training. This builds the dataset from `load_dataset`,
+    one should customize this function to train the model on its own dataset.
 
     Args:
         dataset_name (`str`):
@@ -27,10 +27,10 @@ def build_dataset(dataset_name="imdb"):
             The dataloader for the dataset.
     """
     # load imdb with datasets
-    ds = load_dataset(dataset_name, split="train")
-    ds = ds.rename_columns({"text": "review"})
+    ds = load_dataset(dataset_name, split='train')
+    ds = ds.rename_columns({'text': 'review'})
     ds = ds.select([0, 10, 20, 30, 40, 50])
-    ds = ds.filter(lambda x: len(x["review"]) > 100, batched=False)
+    ds = ds.filter(lambda x: len(x['review']) > 100, batched=False)
     return ds
 
 
@@ -43,20 +43,20 @@ def main():
     input_size = LengthSampler(3, 12)
 
     ds = build_dataset()
-    ds.set_format(type="torch")
+    ds.set_format(type='torch')
 
     def tokenize(sample):
-        sample = tokenizer(sample["review"][:input_size()])
-        sample["query"] = tokenizer.decode(sample["input_ids"])
+        sample = tokenizer(sample['review'][:input_size()])
+        sample['query'] = tokenizer.decode(sample['input_ids'])
         return sample
 
     device = torch.device(
         'cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     # We retrieve the dataloader by calling the `build_dataset` function.
-    ds = build_dataset(dataset_name="imdb")
+    ds = build_dataset(dataset_name='imdb')
     ds = ds.map(tokenize, batched=False)
-    ds.set_format(type="torch", device=device)
+    ds.set_format(type='torch', device=device)
 
     def collator(data):
         return dict((key, [d[key] for d in data]) for key in data[0])
@@ -69,5 +69,5 @@ def main():
         exit()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
