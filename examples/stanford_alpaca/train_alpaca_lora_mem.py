993fe0d7e0d26db9c1d0ff10c43899d95bff253b
jianzhnie <jianzhnie@126.com>
2023-05-22 16:25:44 +0800

    update flash_atten

--- examples/stanford_alpaca/train_alpaca_lora_mem.py
+++ examples/stanford_alpaca/train_alpaca_lora_mem.py
@@ -3,9 +3,10 @@
 # Need to call this before importing transformers.
 
 import sys
+
 sys.path.append('../../')
-from chatgpt.models.llama_flash_attn_monkey_patch import (
-    replace_llama_attn_with_flash_attn, )
+from chatgpt.models.llama_flash_attn_monkey_patch import \
+    replace_llama_attn_with_flash_attn
 
 replace_llama_attn_with_flash_attn()
 
--- examples/stanford_alpaca/train_alpaca_mem.py
+++ examples/stanford_alpaca/train_alpaca_mem.py
@@ -1,8 +1,8 @@
 # Make it more memory efficient by monkey patching the LLaMA model with FlashAttn.
 
 # Need to call this before importing transformers.
-from chatgpt.models.llama_flash_attn_monkey_patch import (
-    replace_llama_attn_with_flash_attn, )
+from chatgpt.models.llama_flash_attn_monkey_patch import \
+    replace_llama_attn_with_flash_attn
 
 replace_llama_attn_with_flash_attn()
 
